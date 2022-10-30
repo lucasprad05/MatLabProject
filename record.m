da0d6aa598970c22ca2be5ca519465f953891701
ZaniAndrei <101280774+ZaniAndrei@users.noreply.github.com>
2022-10-29 22:15:38 -0300

    Update record.m

--- record.m
+++ record.m
@@ -1,4 +1,4 @@
-FFs = 8000;
+Fs = 8000;
 nBits = 16;
 nChannels = 1;
 
@@ -16,19 +16,22 @@ recordblocking(recObj2,recDuration);
 y = getaudiodata(recObj);
 y2 = getaudiodata(recObj2);
 
+t = 0:seconds(1/Fs):seconds(5);
+t = t(1:end-1);
+
 
 soma = y + y2;
-subplot(3, 2, 1);
-plot(y);
-xlim([0 Fs*recDuration]);
-xlabel('Frequencia(hz)');
-ylabel('Decibeis(dB)');
-title('Som');
+%subplot(3, 2, 1);
+%plot(t, y);
+%xlim([0 Fs*recDuration]);
+%xlabel('Tempo(s)');
+%ylabel('Decibeis(dB)');
+%title('Som');
 
-subplot(3, 2, 2);
-plot(y2);
-xlim([0 Fs*recDuration]);
-xlabel('Frequencia(hz)');
+subplot(3, 1, 1);
+plot(t, y2);
+ylim([-0.2 0.2]);
+xlabel('Tempo(s)');
 ylabel('Decibeis(dB)');
 title('Ruido');
 
@@ -38,35 +41,38 @@ title('Ruido');
 %play(p);
 
 subplot(3,1,2);
-plot(soma);
-xlim([0 Fs*recDuration]);
-xlabel('Frequencia(hz)');
+plot(t, soma);
+xlabel('Tempo(s)');
 ylabel('Decibeis(dB)');
 title('Soma')
 
 r = snr(y, y2);
+
 window = 10;
 mask = ones(1, window)/window;
 movingAverage = conv(y2, mask,'same');
 teste = movmean(y2, window);
-
 subplot(3, 1, 2);
-plot(movingAverage);
-xlim([0 Fs*recDuration]);
+plot(t, movingAverage);
+ylim([-0.2 0.2]);
 xlabel('Tempo(s)');
 ylabel('Decibeis(dB)');
 title('Media movel');
 
 cleanFilter = y2 - movingAverage;
 subplot(3, 1, 3);
-
-plot(cleanFilter);
+plot(t, cleanFilter);
+ylim([-0.2 0.2]);
 xlabel('Tempo(s)');
 ylabel('Decibeis(dB)');
 title('Grafico de erro');
+
+
 tocar = audioplayer(movingAverage, Fs);
 tocar2 = audioplayer(y2, Fs);
 
 
 disp("Tocando Ruido...");
 play(tocar);
+
+
